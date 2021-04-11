e3fa64bc3976f01db87bc9d39f6684b3b74faf56
pruthvi03 <pruthvi.hingu012@gmail.com>
2021-04-11 23:56:42 +0530

    resolver edited

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const bcrypt = require('bcryptjs');
 require('dotenv').config();
 const app = express();
 require('./db/mongoose');
 const Event = require('./models/event');
+const User = require('./models/user');
 
 app.use(express.json());
 
@@ -26,17 +28,27 @@ app.use('/graphql',
             price: Float!
             date: String!
         }
+        type User{
+            _id: ID!
+            email: String!
+            password: String
+        }
         input EventInput{
             title: String!
             description: String!
             price: Float!
             date: String!
         }
+        input UserInput{
+            email: String!
+            password: String!
+        }
         type RootQuery{
             events: [Event!]!
         }
         type RootMutation{
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
         schema {
                 query: RootQuery
@@ -46,34 +58,65 @@ app.use('/graphql',
         rootValue: {
             events: () => {
                 return Event.find()
-                .then(events=>{
-                    return events.map(event =>{
-                        return {...event._doc, _id: event._doc._id.toString()};
+                    .then(events => {
+                        return events.map(event => {
+                            return { ...event._doc, _id: event._doc._id.toString() };
+                        })
+                    }).catch(err => {
+                        throw err;
                     })
-                }).catch(err=>{
-                    throw err;
-                })
             },
             createEvent: (args) => {
                 const event = new Event({
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: new Date(args.eventInput.date)
+                    date: new Date(args.eventInput.date),
+                    creator: "60733bd14718da3bbc88b4f6"
                 });
+                let createdEvent;
                 return event.save()
                     .then(result => {
-                        console.log(result);
-                        return {...result._doc, _id: event._doc._id.toString()};
+                        createdEvent = { ...result._doc, _id: event._doc._id.toString() };
+                        return User.findOne({_id:'60733bd14718da3bbc88b4f6'});
                     })
-                    .catch(err=>{
+                    .then(user=>{
+                        if (!user){
+                            throw new Error('User exists already');
+                        }
+                        user.createdEvents.push(event);
+                        return user.save();
+                    })
+                    .then(result=>{
+                        return createdEvent;
+                    })
+                    .catch(err => {
                         console.log(err);
                         throw err;
                     });
-                
-            }
-        },
-        graphiql: true
+
+            },
+            createUser: async (args) => {
+                try {
+                    const userExist = await User.findOne({email:args.userInput.email});
+                    if (userExist){
+                        throw new Error('User exists already');
+                    }
+                    const hashedPassword = await bcrypt.hash(args.userInput.password, 12);
+                    const user = new User({
+                        email: args.userInput.email,
+                        password: hashedPassword
+                    });
+                    const result = await user.save();
+                    return { ...result._doc, password: null, _id: result._doc._id.toString() }
+                } catch (error) {
+                    throw error
+                }
+
+
+            },
+            graphiql: true
+        }
     })
 );
 
